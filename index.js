58238fb1ecb69bcec317b06a9279dc35da89d5a2
jnakht <jisan.cse.cu@gmail.com>
2025-09-21 21:06:33 +0600

    port issue solved

--- index.js
+++ index.js
@@ -462,8 +462,8 @@ app.get('/', (req, res) => {
     res.send("canvascraze server is running now...");
 });
 
-// app.listen(port, ()=> {
-//     console.log(`Server is running on port: ${port}`);
-// });
+app.listen(port, ()=> {
+    console.log(`Server is running on port: ${port}`);
+});
 
 module.exports = app;
